bb7093e274d3a6cd29f3af777bb630c40c3479c3
Nattida Wong <nattidac.wong@gmail.com>
2020-12-02 22:30:16 -0500

    commit tree b7b992d46d45a3f7f9eac56a24c9321cfc96c3de

--- app.rb
+++ app.rb
@@ -2,5 +2,29 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @reverse_name = params[:name].reverse
+    "#{@reverse_name}"
+  end
+
+  get '/square/:number' do
+    @squared = params[:number].map { |e| e ** 2 }.to_s
+    "#{@squared}"
+  end
+
+  get '/say/:number/:phrase' do
+    @say_phrase = params[:number] * params[:phrase] 
+    "#{@say_phrase}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @say = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5].to_s
+    "#{@say}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:number1].to_s + params[:number2].to_s
+    "#{@operation}"
+  end
 
 end
